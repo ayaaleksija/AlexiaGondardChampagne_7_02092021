d377390b6422f3670c67e01915200e0b937af86e
Alexia Gondard-Champagne <a.gondardchamp@gmail.com>
2021-09-16 23:34:17 +0200

    ajout des routes comment

--- backend/routes/comments.js
+++ backend/routes/comments.js
@@ -5,6 +5,7 @@ const auth = require('../middleware/auth');
 const commentCtrl = require('../controllers/commentCtrl');
 
 router.post('/', auth, commentCtrl.createComment);
-//router.get('/', auth, commentCtrl.getAllComment);
+router.get('/', auth, commentCtrl.getAllComment);
+router.delete('/:id', auth, postCtrl.deleteComment);
 
 module.exports = router;
